aa8e56f217467268b7e5f0b03f0f67463e3ac9eb
Nikita Pursani <nikitapursani@gmail.com>
2024-02-01 14:57:06 +0530

    Add state

--- src/App.js
+++ src/App.js
@@ -1,32 +1,50 @@
-import Component from 'react';
+import { Component } from 'react';
 
 import logo from './logo.svg';
 import './App.css';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p></p>
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
 class App extends Component {
-  
+  constructor() {
+    super();
+
+    this.state = {
+      name: 'Nikita1'
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Nikita</p>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Hi {this.state.name}</p>
+          <button> Change Name </button>
         </header>
       </div>
     );
   }
-
 }
 
 export default App;
