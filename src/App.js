c1189aa2a4d392dbaa7ecec247dd1531bd5e5464
Nikita Pursani <nikitapursani@gmail.com>
2024-02-05 13:47:46 +0530

    add componentDidMount method

--- src/App.js
+++ src/App.js
@@ -1,65 +1,39 @@
 import { Component } from 'react';
-
 import logo from './logo.svg';
 import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p></p>
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      name: {firstName: 'Nikita', lastName: 'Pursani'},
-      company: 'ZTM'
+        monsters: []
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then((users) => this.setState(() => {
+        return {monsters: users}
+      },
+      () => {
+        console.log(this.state)
+      }))
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          
-          <p>Hi {this.state.name.firstName} {this.state.name.lastName}, {this.state.company}</p>
-
-          <button onClick={() => {
-            this.setState(
-              () => {
-                return {
-                  name: {firstName: 'Andrei', lastName: "Neaigoi"}
-                };
-              }, 
-              () => {
-                console.log(this.state);  // this callback runs after the state changes ve been applied
-              }
+        {
+          this.state.monsters.map((monster) => {
+            return (
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+              </div>
             );
-            
-          }}> 
-            Change Name 
-          </button>
-
-        </header>
+          })
+        }
       </div>
     );
   }
