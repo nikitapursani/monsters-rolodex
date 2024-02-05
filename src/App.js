92854a0d42c2bf742af80610d31f2545d4d93ffb
Nikita Pursani <nikitapursani@gmail.com>
2024-02-05 15:32:42 +0530

    My solution to searching and filtering

--- src/App.js
+++ src/App.js
@@ -35,6 +35,28 @@ class App extends Component {
         placeholder='search monsters' 
         onChange={(event) => {
           console.log(event);
+
+          // Get the input text
+          const inputText = event.target.value;
+
+          // Get the monsters from the API each time a 
+          // change occurs in the value
+          fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then((monsters) => {
+
+              // Filter the monsters to get the only
+              // the ones that start with the input text
+              monsters = monsters.filter((monster) => monster.name.startsWith(inputText))
+
+              // setState to the new filtered monsters array
+              this.setState(() => {
+                return {monsters: monsters};
+              },
+              () => {
+                console.log(this.state);
+              })
+            })
         }}/>
 
         {
