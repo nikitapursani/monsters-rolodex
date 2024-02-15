dd8052181e2dd62c0dbdee02d33fe2e906e4b0a6
Nikita Pursani <nikitapursani@gmail.com>
2024-02-15 16:17:30 +0530

    change card-list to functional component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,24 +1,17 @@
-import { Component } from 'react';
 import Card from '../card/card.component';
-
 import './card-list.styles.css'
 
-class CardList extends Component {
-    render() {
-		const { monsters } = this.props;
-
-		return (
-			<div className='card-list'>
-				{
-                    monsters.map((monster) => {
-                        return(
-                            <Card monster={monster}/>
-                        )
-                    })
-                }
-			</div>
-		);
-	}
-}
+// Actaully the argument is props, but we are destructing it in the defination itself, and since there is nothing at the top we can do an implicit return
+const CardList = ({monsters}) => (
+    <div className='card-list'>
+        {
+            monsters.map((monster) => {
+                return(
+                    <Card monster={monster}/>
+                )
+            })
+        }
+    </div>
+);
 
 export default CardList;
