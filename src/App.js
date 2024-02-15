71516b066a19ce95ddb325c4f820be4c3e23d447
Nikita Pursani <nikitapursani@gmail.com>
2024-02-15 15:52:53 +0530

    add useEffect call for filtering monsters, adding efficiency

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const App = () => {
   // This is how the state is used in the context of a functional component, here state is not an object. For each value in the state, you'll have to call useState(). What is passed into the useState() is the initial value you want that value to have. It returns the value and the setter for that value in the state.
   const [searchField, setsearchField] = useState(''); // [value, setValue]
   const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
   // Produces a side effect
   // Takes a callback that runs the very first time the component is rendered, and it only ever runs again when the variables, that are the dependencies, that are passed as the second argument change. The second argument is an array of variables.
   // So, in this case, it only ever runs once, only when the component is first rendered
@@ -24,12 +25,17 @@ const App = () => {
     // setState to the new searchField from the input search box
     setsearchField(searchField);
   }
-  // Filter the monsters using the searchField before sending it to the UI
-  const filteredMonsters = monsters.filter((monster) => {
-    return monster.name
-      .toLowerCase()
-      .includes(searchField)
-  });
+  // Monsters are filtered only when monsters or searchField is updated for efficiency
+  useEffect(() => {
+    // Filter the monsters using the searchField before sending it to the UI
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name
+        .toLowerCase()
+        .includes(searchField)
+    });
+    // Set filteredMonsters to newFilteredMonsters in the state
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
   // All of this is rendered as the UI
   return(
     <div className="App">
