35a71e92d2b45c7222401edf042d1647b3962cf5
Nikita Pursani <nikitapursani@gmail.com>
2024-02-15 15:48:29 +0530

    Clean Up

--- src/App.js
+++ src/App.js
@@ -5,39 +5,18 @@ import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
 const App = () => {
-
-  console.log('render');
-
   // This is how the state is used in the context of a functional component, here state is not an object. For each value in the state, you'll have to call useState(). What is passed into the useState() is the initial value you want that value to have. It returns the value and the setter for that value in the state.
   const [searchField, setsearchField] = useState(''); // [value, setValue]
   const [monsters, setMonsters] = useState([]);
-  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
-  const [stringField, setStringField] = useState('');
-
   // Produces a side effect
   // Takes a callback that runs the very first time the component is rendered, and it only ever runs again when the variables, that are the dependencies, that are passed as the second argument change. The second argument is an array of variables.
   // So, in this case, it only ever runs once, only when the component is first rendered
   useEffect(() => {
     // Fetch the monters using the api and set the monsters in state to the ones we got from the api
-    console.log('effect fired');
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then((users) => setMonsters(users));
   }, []);
-
-  useEffect(() => {
-    // Filter the monsters to get the only the ones where thier name includes the input text
-    const newFilteredMonsters = monsters.filter((monster) => {
-      return monster.name
-        .toLowerCase()
-        .includes(searchField)
-    });
-
-    setFilteredMonsters(newFilteredMonsters);
-
-    console.log('effect is firing');
-  }, [monsters, searchField]);
-
   // The function that is passed to the searchbox component which changes the searchField in the state so that the cardlist component will be rerendered with the new input value and a new list of monsters that match the searchField
   const onSearchChange = (event) => {
     // Get the input text
@@ -45,14 +24,12 @@ const App = () => {
     // setState to the new searchField from the input search box
     setsearchField(searchField);
   }
-
-  const onStringChange = (event) => {
-    setStringField(event.target.value);
-  }
-
-  console.log(filteredMonsters);
-
-
+  // Filter the monsters using the searchField before sending it to the UI
+  const filteredMonsters = monsters.filter((monster) => {
+    return monster.name
+      .toLowerCase()
+      .includes(searchField)
+  });
   // All of this is rendered as the UI
   return(
     <div className="App">
@@ -62,16 +39,7 @@ const App = () => {
         onChangeHandler = {onSearchChange} 
         placeholder = 'search monsters'
       /> 
-
-      <SearchBox
-        onChangeHandler = {onStringChange} 
-        placeholder = 'set string'
-      />
-      
       <CardList monsters={filteredMonsters} />
-
-      {/*
-      */}
     </div>
   );
 };
