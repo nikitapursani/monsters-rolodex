9cbbf2b569ed99b023d45bfb1d6bf2f26d1ec50b
Nikita Pursani <nikitapursani@gmail.com>
2024-02-15 16:48:54 +0530

    futher destructing the props to monster to name, email and id

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,18 +1,15 @@
 import './card.styles.css'
 
-const Card = ({monster}) => {
-  const { name, email, id } = monster;
-  // UI
-  return (
-    <div className="card-container" key={id}>
-      <img 
-        alt={`monster ${name}`} 
-        src={`https://robohash.org/${id}?set=set2&size=180x180`}
-      />
-      <h2>{name}</h2>
-      <p>{email}</p>
-    </div>
-  );
-}
+// Here, the monster is destructed from props, which is futher destructed into name, email and id
+const Card = ({monster: { name, email, id }}) => (
+  <div className="card-container" key={id}>
+    <img 
+      alt={`monster ${name}`} 
+      src={`https://robohash.org/${id}?set=set2&size=180x180`}
+    />
+    <h2>{name}</h2>
+    <p>{email}</p>
+  </div>
+);
 
 export default Card;
